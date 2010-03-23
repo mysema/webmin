28cde8bfa53bb66c17767fdf121a4b15e93608bc
Timo Westkämper <timo.westkamper@mysema.com>
2010-03-23 09:22:54 +0000

    refactored packages

---
+++ src/main/java/com/mysema/webmin/conf/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2010 Mysema Ltd.
+ * All rights reserved.
+ * 
+ */
+@DefaultAnnotation( { Nonnull.class })
+package com.mysema.webmin.conf;
+
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+
